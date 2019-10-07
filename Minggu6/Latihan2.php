f82b102aff0a540c18ff26cbb34026cfb4757736
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2019-10-07 11:41:21 +0700

    Latihan 2 : Kalkulatr

---
+++ Minggu6/Latihan2.php
@@ -0,0 +1,35 @@
+<?php
+//Kalkulator Latihan 2
+$a = 15;
+$b = 5;
+
+echo "bilangan 1 = $a"; echo"<br>";
+echo "bilangan 2 = $b"; echo"<br>";
+echo "Berikut hasil perhitungan dari setiap operator"; echo "<br/>";
+
+echo "<br/> PENJUMLAHAN";
+$c = $a + $b;
+echo "operator = +"; echo "<br>";
+echo "hasilnya : $a + $b = $c";
+echo "<br/>";
+echo "<br/> PENGURANGAN";
+$d = $a - $b;
+echo "operator = -"; echo "<br>";
+echo "hasilnya : $a - $b = $d";
+echo "<br/>";
+echo "<br/> PERKALIAN";
+$e = $a * $b;
+echo "operator = *"; echo "<br>";
+echo "hasilnya : $a * $b = $e";
+echo "<br/>";
+echo "<br/> PEMBAGIAN";
+$f = $a / $b;
+echo "operator = /"; echo "<br>";
+echo "hasilnya : $a / $b = $f";
+echo "<br/>";
+echo "<br/> PENJUMLAHAN";
+$g = $a % $b;
+echo "operator = %"; echo "<br>";
+echo "hasilnya : $a + $b = $g";
+echo "<br/>";
+?>
