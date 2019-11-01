b04294c19a6c0ccd4976728b084d826e5e618b30
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2019-11-01 07:41:14 +0700

    Penambahan Fungsi Search dan edit edit tipis yang lain, tipis doang kok bang

--- Pertemuan-8/Tugas/Tambahin.php
+++ Pertemuan-8/Tugas/Tambahin.php
@@ -11,15 +11,21 @@
             $jurusan = $_POST["id_jur"];
             $conn=mysqli_connect ("localhost","root","") or die ("koneksi gagal");
             mysqli_select_db($conn,"lomba");
+            $sqlstr="insert into mahasiswa (NRP,nama,alamat,id_jur)
+            values ('$NRP','$nama','$alamat','$jurusan')";
+            $hasil = mysqli_query($conn,$sqlstr);
+            
+            if($hasil) echo "<h3>Simpan data mahasiswa berhasil dilakukan</h3><br>";
+            else echo "<h3>Gagal</h3><br>";
+
+            mysqli_select_db($conn,"lomba");
+            $hasil=mysqli_query($conn,"select nama from jurusan where id_jur='$jurusan'");
+            $row=mysqli_fetch_array($hasil);
+            $jurusan=$row[0];
             echo "NRP : $NRP <br>";
             echo "Nama : $nama <br>";
             echo "Alamat : $alamat <br>";
             echo "Jurusan : $jurusan <br>";
-            $sqlstr="insert into mahasiswa (NRP,nama,alamat,id_jur)
-            values ('$NRP','$nama','$alamat','$jurusan')";
-            $hasil = mysqli_query($conn,$sqlstr);
-            if($hasil) echo "Simpan data mahasiswa berhasil dilakukan<br>";
-            else echo "Gagal";
         ?>
         <a href="dataMahasiswa.html">Return to Home</a>
     </BODY>
